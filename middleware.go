238f9d9a62c0b2d5cc1fc4aeb42965035a8cc921
Christoph Probst <38609485+prochri@users.noreply.github.com>
2022-12-18 11:56:54 +0100

    merged original repository

--- middleware.go
+++ middleware.go
@@ -19,9 +19,18 @@ func ResetLookup() {
 	lookup = nil
 }
 
+type LocationRewrite struct {
+	IpRange string `json:"ipRange"`
+	Country string `json:"country,omitempty"`
+	Region  string `json:"region,omitempty"`
+	City    string `json:"city,omitempty"`
+	IPnet   *net.IPNet
+}
+
 // Config the plugin configuration.
 type Config struct {
-	DBPath string `json:"dbPath,omitempty"`
+	DBPath           string            `json:"dbPath,omitempty"`
+	LocationRewrites []LocationRewrite `json:"locationRewrites,omitempty"`
 }
 
 // CreateConfig creates the default plugin configuration.
@@ -33,17 +42,37 @@ func CreateConfig() *Config {
 
 // TraefikGeoIP2 a traefik geoip2 plugin.
 type TraefikGeoIP2 struct {
-	next http.Handler
-	name string
+	next             http.Handler
+	lookup           LookupGeoIP2
+	name             string
+	locationRewrites []LocationRewrite
 }
 
 // New created a new TraefikGeoIP2 plugin.
 func New(_ context.Context, next http.Handler, cfg *Config, name string) (http.Handler, error) {
+	var err error
+	for i := range cfg.LocationRewrites {
+		_, cfg.LocationRewrites[i].IPnet, err = net.ParseCIDR(cfg.LocationRewrites[i].IpRange)
+		if err != nil {
+			break
+		}
+	}
+	if err != nil {
+		return &TraefikGeoIP2{
+			lookup:           nil,
+			next:             next,
+			name:             name,
+			locationRewrites: cfg.LocationRewrites,
+		}, nil
+	}
+
 	if _, err := os.Stat(cfg.DBPath); err != nil {
 		log.Printf("[geoip2] DB not found: db=%s, name=%s, err=%v", cfg.DBPath, name, err)
 		return &TraefikGeoIP2{
-			next: next,
-			name: name,
+			lookup:           nil,
+			next:             next,
+			name:             name,
+			locationRewrites: cfg.LocationRewrites,
 		}, nil
 	}
 
@@ -68,8 +97,10 @@ func New(_ context.Context, next http.Handler, cfg *Config, name string) (http.H
 	}
 
 	return &TraefikGeoIP2{
-		next: next,
-		name: name,
+		lookup:           lookup,
+		next:             next,
+		name:             name,
+		locationRewrites: cfg.LocationRewrites,
 	}, nil
 }
 
@@ -88,13 +119,18 @@ func (mw *TraefikGeoIP2) ServeHTTP(reqWr http.ResponseWriter, req *http.Request)
 		ipStr = tmp
 	}
 
-	res, err := lookup(net.ParseIP(ipStr))
+	// start conflict
+	ip := net.ParseIP(ipStr)
+	res, err := mw.lookup(ip)
 	if err != nil {
-		log.Printf("[geoip2] Unable to find: ip=%s, err=%v", ipStr, err)
-		res = &GeoIPResult{
-			country: Unknown,
-			region:  Unknown,
-			city:    Unknown,
+		res, err = mw.findLocalRewrite(ip)
+		if err != nil {
+			log.Printf("[geoip2] Unable to find for `%s', %v", ipStr, err)
+			res = &GeoIPResult{
+				country: Unknown,
+				region:  Unknown,
+				city:    Unknown,
+			}
 		}
 	}
 
@@ -103,4 +139,17 @@ func (mw *TraefikGeoIP2) ServeHTTP(reqWr http.ResponseWriter, req *http.Request)
 	req.Header.Set(CityHeader, res.city)
 
 	mw.next.ServeHTTP(reqWr, req)
+}
+
+func (mw *TraefikGeoIP2) findLocalRewrite(ip net.IP) (*GeoIPResult, error) {
+	for _, lr := range mw.locationRewrites {
+		if lr.IPnet.Contains(ip) {
+			return &GeoIPResult{
+				country: lr.Country,
+				region:  lr.Region,
+				city:    lr.City,
+			}, nil
+		}
+	}
+	return nil, geoip2.ErrNotFound
 }
